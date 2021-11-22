1e58774dc581bfd2f7c9985f7c8c6322e9adda34
martinabrle <martinabrle@users.noreply.github.com>
2021-11-22 07:59:34 +0100

    Update to a new version of node-fetch, switching to import from require

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 //first version of every index.js: console.log('Welcome to this application');
-const fetch = require('node-fetch')
-const _ = require('lodash');
-const path = require('path');
-const fs = require('fs');
+import fetch from "node-fetch"; //this one does not work anymore const fetch = require('node-fetch')
+import _ from "lodash"; //const _ = require('lodash');
+import path from "path"; //const path = require('path');
+import fs from "fs"; //const fs = require('fs');
 
 async function run() {
   const response = await fetch("https://dev.to/api/articles?state=rising");
@@ -11,7 +11,7 @@ async function run() {
   const top5 = _.take(sorted, 3);
 
   const filePrefix = new Date().toISOString().split('T')[0];
-  fs.writeFileSync(path.join(__dirname, `${filePrefix}-feed.json`), JSON.stringify(top5, null, 2));
+  fs.writeFileSync(path.resolve(`${filePrefix}-feed.json`), JSON.stringify(top5, null, 2)); //const __dirname = path.resolve();
  }
 
  run();
