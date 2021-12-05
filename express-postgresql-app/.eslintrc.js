8a4fc788f27afefbccc786cfca015a52831b4964
martinabrle <71389217+martinabrle@users.noreply.github.com>
2021-12-05 18:52:03 +0100

    Update and rename .eslintrc.json to .eslintrc.js

---
+++ express-postgresql-app/.eslintrc.js
@@ -0,0 +1,33 @@
+/*
+.eslintrc.js
+*/
+const ERROR = 2;
+const WARN = 1;
+
+module.exports = {
+  extends: "eslint:recommended",
+  env: {
+    es6: true
+  },
+  overrides: [
+    {
+      files: [
+        "**/*.test.js"
+      ],
+      env: {
+        jest: true // now **/*.test.js files' env has both es6 *and* jest
+      },
+      // Can't extend in overrides: https://github.com/eslint/eslint/issues/8813
+      // "extends": ["plugin:jest/recommended"]
+      plugins: ["jest"],
+      rules: {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
+      }
+    }
+  ],
+};
+
