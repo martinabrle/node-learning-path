aee36da1b6a6482320fba90ed3b865fcfb7e7781
martinabrle <71389217+martinabrle@users.noreply.github.com>
2021-12-05 21:41:23 +0100

    Update index.js

--- express-postgresql-app/src/index.js
+++ express-postgresql-app/src/index.js
@@ -1,11 +1,42 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+function isAuthorized(req,res, next) {
+    const auth = req.headers.authorization;
+    if (auth === "supersecret") {
+        next();
+    } else {
+        res.status(401);
+        res.send("Not permitted!");
+    }
+}
+app.get('/', (req, res) => res.send('Hello world!!!'));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.get('/users', isAuthorized, (req,res) => {
+    res.json([{
+      id: 1,
+      name: 'User Userson'
+    }])
+  });
+  
+app.get("/products", (req,res) => {
+    const products = [
+    {
+      id: 1,
+      name: "hammer",
+    },
+    {
+      id: 2,
+      name: "screwdriver",
+    },
+    {
+      id: 3,
+      name: "wrench",
+    }
+   ];
+  
+   res.json(products);
+  });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
