d89a70a587e6f91fb8f9c38b7f97d90d369ffe3b
martinabrle <71389217+martinabrle@users.noreply.github.com>
2021-12-10 20:23:54 +0100

    Update index.js

--- express-postgresql-app/src/index.js
+++ express-postgresql-app/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000
+/*eslint node/no-process-env: "off"*/
+const port = process.env.PORT || 3000;
 
 function isAuthorized(req,res, next) {
     const auth = req.headers.authorization;
