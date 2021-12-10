c8f731d003c638318e39413da4939e2bdf08ad28
martinabrle <71389217+martinabrle@users.noreply.github.com>
2021-12-10 20:29:25 +0100

    Update .eslintrc.js

--- express-postgresql-app/.eslintrc.js
+++ express-postgresql-app/.eslintrc.js
@@ -26,7 +26,8 @@ module.exports = {
         "jest/no-focused-tests": "error",
         "jest/no-identical-title": "error",
         "jest/prefer-to-have-length": "warn",
-        "jest/valid-expect": "error"
+        "jest/valid-expect": "error",
+        "jest/no-process-env": "off",
       }
     }
   ],
